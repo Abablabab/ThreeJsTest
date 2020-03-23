43156c60de2ea815fa2eb91af220075c2124113f
ross <Abablabab@users.noreply.github.com>
2020-03-23 04:40:55 +0000

    nuke it from orbit
    Let someone else's brain do the work

--- index2.js
+++ index2.js
@@ -1,86 +1,82 @@
 import * as THREE from './modules/three.module.js';
 //import {ColladaLoader} from './modules/ColladaLoader.js';
-//import {OrbitControls} from './modules/OrbitControls.js';
+import {OrbitControls} from './modules/OrbitControls.js';
+
+var camera, controls, scene, renderer;
+
+init();
+animate(); // or render() for static scenes?
 
 function init() {
-    // First we need to set up Three
-    const canvas = document.querySelector('#c');
-    const renderer = new THREE.WebGLRenderer({canvas});
 
-    // Define camera params and create
-    const fov = 45;
-    const aspect = 2;
-    const near = 0.1;
-    const far = 1000;
-    const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
-    camera.position.set(0,10,0);
+    // Three setup
+    scene = new THREE.Scene();
+    scene.background = new THREE.Color( 0xcccccc );
+    scene.fog = new THREE.FogExp2( 0xcccccc, 0.002 );
 
-    // Set up some controls
-    //const controls = new OrbitControls(camera, canvas);
-    //controls.target.set(0, 5, 0);
-    //controls.update();
+    renderer = new THREE.WebGLRenderer( { antialias: true } );
+    renderer.setPixelRatio( window.devicePixelRatio );
+    renderer.setSize( window.innerWidth, window.innerHeight );
+    document.body.appendChild( renderer.domElement );
 
-    // Now set up the scene
-    const scene = new THREE.Scene();
-    scene.background = new THREE.Color('blue');
+    camera = new THREE.PerspectiveCamera( 60, window.innerWidth / window.innerHeight, 1, 1000 );
+    camera.position.set( 400, 200, 0 );
 
-    // Give us a checkerboard base to orientate from
-    {
-        // First the texture
-        const planeSize = 40;
-        const loader = new THREE.TextureLoader();
-        const texture = loader.load('./images/checker.png');
-        texture.wrapS = THREE.RepeatWrapping; // horizontal
-        texture.wrapT = THREE.RepeatWrapping; // vertical
-        texture.magFilter = THREE.NearestFilter;
-        const repeats = planeSize / 2; // Our texture is 2px, so half size
-        texture.repeat.set(repeats, repeats);
+	// controls
+	controls = new OrbitControls( camera, renderer.domElement );
 
-        // Then the textured mesh
-        const planeGeometry = new THREE.PlaneBufferGeometry(planeSize, planeSize);
-        const planeMaterial = new THREE.MeshPhongMaterial({
-            map: texture,
-            side: THREE.DoubleSide,
-        });
-        const mesh = new THREE.Mesh(planeGeometry,planeMaterial);
-        mesh.rotation.x = Math.Pi * -.5; // Great, radian maths.
+	//controls.addEventListener( 'change', render ); // call this only in static scenes (i.e., if there is no animation loop)
+    controls.enableDamping = true; // an animation loop is required when either damping or auto-rotation are enabled
+    controls.dampingFactor = 0.05;
+    controls.screenSpacePanning = false;
+    controls.minDistance = 100;
+    controls.maxDistance = 500;
+    controls.maxPolarAngle = Math.PI / 2;
 
-        scene.add(mesh);
+    // world
+    var geometry = new THREE.CylinderBufferGeometry( 0, 10, 30, 4, 1 );
+	var material = new THREE.MeshPhongMaterial( { color: 0xffffff, flatShading: true } );
+
+    for ( var i = 0; i < 500; i ++ ) {
+        var mesh = new THREE.Mesh( geometry, material );
+        mesh.position.x = Math.random() * 1600 - 800;
+        mesh.position.y = 0;
+        mesh.position.z = Math.random() * 1600 - 800;
+        mesh.updateMatrix();
+        mesh.matrixAutoUpdate = false;
+        scene.add( mesh );
     }
 
-    // Now we need lights to see what we're doing
-    {
-        const skyColor = 0xB1E1FF;  // light blue
-        const groundColor = 0xB97A20;  // brownish orange
-        const intensity = 1;
-        const light = new THREE.HemisphereLight(skyColor, groundColor, intensity);
-        scene.add(light);
-    }
+	// lights
+    var light = new THREE.DirectionalLight( 0xffffff );
+    light.position.set( 1, 1, 1 );
+    scene.add( light );
 
-    // More light, can this be the problem??
-    {
-        const color = 0xFFFFFF;
-        const intensity = 1;
-        const light = new THREE.DirectionalLight(color, intensity);
-        light.position.set(5, 10, 2);
-        scene.add(light);
-        scene.add(light.target);
-    }
+    var light = new THREE.DirectionalLight( 0x002288 );
+    light.position.set( - 1, - 1, - 1 );
+    scene.add( light );
+
+    var light = new THREE.AmbientLight( 0x222222 );
+    scene.add( light );
+
+    //
+    window.addEventListener( 'resize', onWindowResize, false );
+
+}
+
+function onWindowResize() {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+
+    renderer.setSize( window.innerWidth, window.innerHeight );
+}
+
+function animate() {
+    requestAnimationFrame( animate );
+    controls.update(); // only required if controls.enableDamping = true, or if controls.autoRotate = true
+    render();
 }
 
 function render() {
-    const canvas = renderer.domElement;
-    const width = canvas.clientWidth;
-    const height = canvas.clientHeight;
-    const needResize = canvas.width !== width || canvas.height !== height;
-    if (needResize) {
-        renderer.setSize(width, height, false);
-        const canvas = renderer.domElement;
-        camera.aspect = canvas.clientWidth / canvas.clientHeight;
-        camera.updateProjectionMatrix();
-    }
-    renderer.render(scene, camera);
-    requestAnimationFrame(render);
+    renderer.render( scene, camera );
 }
-
-init();
