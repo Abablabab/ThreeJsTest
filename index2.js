fae98c3e59918d98e53715eba445358d50e26d33
ross <ross@abablabab.co.uk>
2020-03-23 02:33:27 +0000

    Try again with a whole different take

---
+++ index2.js
@@ -0,0 +1,69 @@
+import * as THREE from './modules/three.module.js';
+//import {ColladaLoader} from './modules/ColladaLoader.js';
+import {OrbitControls} from './modules/OrbitControls.js';
+
+// First we need to set up Three
+const canvas = document.querySelector('#c');
+const renderer = new THREE.WebGlRenderer({canvas});
+
+// Define camera params and create
+const fov = 45;
+const aspect = 2;
+const near = 0.1;
+const far = 1000;
+const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
+camera.position.set(0,10,0);
+
+// Set up some controls
+const controls = new OrbitControls(camera, canvas);
+controls.target.set(0, 5, 0);
+controls.update();
+
+// Now set up the scene
+const scene = new THREE.Scene();
+scene.background = new THREE.Colour('blue');
+
+// Give us a checkerboard base to orientate from
+{
+    // First the texture
+    const planeSize = 40;
+    const loader = new THREE.TextureLoader();
+    const texture = loader.load('./images/checker.png');
+    texture.wrapS = THREE.RepeatWrapping; // horizontal
+    texture.wrapT = THREE.RepeatWrapping; // vertical
+    texture.magFilter = THREE.NearestFilter;
+    const repeats = planeSize / 2; // Our texture is 2px, so half size
+    texture.repeat.set(repeats, repeats);
+
+    // Then the textured mesh
+    const planeGeometry = new THREE.PlaneBufferGeometry(planeSize, planeSize);
+    const planeMaterial = new THREE.MeshPhongMaterial({
+        map: texture,
+        side: THREE.DoubleSide,
+    });
+    const mesh = new THREE.Mesh(planeGeo,planeMat);
+    mesh.rotation.x = Math.Pi * -.5; // Great, radian maths. 
+
+    scene.add(mesh);
+}
+
+function doNeedResize(renderer) {
+    const canvas = renderer.domElement;
+    const width = canvas.clientWidth;
+    const height = canvas.clientHeight;
+    const needResize = canvas.width !== width || canvas.height !== height;
+    if (needResize) {
+      renderer.setSize(width, height, false);
+    }
+    return needResize;    
+}
+
+function render() {
+    if (doNeedResize(renderer) {
+        const canvas = renderer.domElement;
+        camera.aspect = canvas.clientWidth / canvas.clientHeight;
+        camera.updateProjectionMatrix();
+    }
+    renderer,render(scene, camera);
+    requestAnimationFrame(render);
+}
